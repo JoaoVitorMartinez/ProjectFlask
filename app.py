1f395ccaea7c57431b58679c42db57992456c4c1
vitor <justdatascientist@gmail.com>
2022-08-10 22:09:40 -0300

    Projeto mais simples com Flask

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask("Projeto")
 @app.route("/")
 def ola_mundo():
-    return "Olá mundo Flask!!"
+    nome = "Joao"
+    produtos = [
+        {"nome" : "Caneta", "preco" : 0.99},
+         {"nome" : "Xbox One", "preco" : 1600.00}
+         ]
+     
+
+    return render_template("index.html",n= nome, aProdutos=produtos), 200
 
 app.run()
