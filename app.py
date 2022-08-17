b886f1a6885f919e00bbe801b12c1ecb373dfa2b
vitor <justdatascientist@gmail.com>
2022-08-17 20:53:24 -0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from distutils.log import debug
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask("Projeto")
@@ -18,6 +17,26 @@ def main():
 @app.route("/teste/<var>")
 def teste(var=""):
     return "Nova rota teste: {}".format(var), 200
+
+@app.route("/outra")
+@app.route("/outra/<var>")
+def outra(var= ""):
+    return "Var: {}".format(var), 200
+
+# Rota formulário
+@app.route("/form")
+def form():
+    return render_template("form.html"), 200
+
+# Rota tratamento do formulário
+@app.route("/form_recebe", methods=["GET", "POST"])
+def form_recebe():
+    nome = ""
+    if request.method == "POST":
+        nome = request.form["nome"]
+        return "Nome: {}".format(nome), 200
+    else:
+        return "Não pode chamar direto no GET", 200
 
 
 app.run()
