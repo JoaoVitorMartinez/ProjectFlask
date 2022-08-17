b809b2b36d8e7192de27876bd2c897670d022583
vitor <justdatascientist@gmail.com>
2022-08-17 19:52:05 -0300

    commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from distutils.log import debug
 from flask import Flask, render_template
 
 
 app = Flask("Projeto")
 @app.route("/")
-def ola_mundo():
-    nome = "Joao"
+def main():
+    nome = "variável"
     produtos = [
         {"nome" : "Caneta", "preco" : 0.99},
          {"nome" : "Xbox One", "preco" : 1600.00}
@@ -12,5 +13,11 @@ def ola_mundo():
      
 
     return render_template("index.html",n= nome, aProdutos=produtos), 200
+
+@app.route("/teste")
+@app.route("/teste/<var>")
+def teste(var=""):
+    return "Nova rota teste: {}".format(var), 200
+
 
 app.run()
